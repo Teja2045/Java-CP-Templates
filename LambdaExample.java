68d51105b7e8afb930b01cd3d3b37ceaedac0cd0
saiteja <tejargukt123@gmail.com>
2023-07-29 23:56:32 +0530

    added lamdaexample

---
+++ LambdaExample.java
@@ -0,0 +1,29 @@
+
+interface IPrinter {
+    void print(long area);
+}
+
+class Square {
+    long side;
+
+    public Square(long side) {
+        this.side = side;
+    }
+
+    public long area() {
+        return side * side;
+    }
+
+    public void print(IPrinter printer) {
+        printer.print(area());
+    }
+}
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        Square sq = new Square(10);
+        sq.print((x) -> {
+            System.out.println("area=" + x);
+        });
+    }
+}
