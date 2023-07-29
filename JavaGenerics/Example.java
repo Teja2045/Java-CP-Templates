501041ccba6fcd62ad3cd5cad5c25c4a0351caf9
saiteja <tejargukt123@gmail.com>
2023-07-30 01:13:08 +0530

    more generics example

--- JavaGenerics/Example.java
+++ JavaGenerics/Example.java
@@ -1,12 +1,24 @@
 package JavaGenerics;
 
-public class Example {
-    void print() {
+// extra example
+class E<T> {
+    T t;
 
+    public E(T t) {
+        this.t = t;
+    }
+}
+
+public class Example {
+    <T> void print(T t) {
     }
 
     public static void main(String[] args) {
         Sorter<Integer> sr = new Sorter<Integer>(new Integer[] { 1, 2, 3, 4, 5 });
         sr.sort();
+
+        // extra example
+        E<int[]> e = new E<int[]>(new int[] { 1, 2, 3 });
+        e.t[0] = 10;
     }
 }
